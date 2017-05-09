41ac4b1c5cd66f248d78fdf849d6ece9b9da35ed
Sylvester Klirowski <Sly@Sylvesters-MacBook-Pro.fritz.box>
2017-05-09 15:24:31 +0200

    added contact reply logic

--- js/contact-form-scripts.js
+++ js/contact-form-scripts.js
@@ -3,18 +3,12 @@
 	GITHUB: https://github.com/voice4eva/
 */
 
-$("#contactFormFR").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Avez-vous rempli le formulaire correctement?");
-    } else {
-        // everything looks good!
+$("#contactFormFR").submit(function (event) {
         event.preventDefault();
         submitForm();
-    }
+        $("#contactFormFR")[0].reset();
+        submitMSG(true, "Merci. Nous avons envoyé une confirmation à votre adresse e-mail. (Vérifiez le dossier spam si vous ne le voyez pas dans votre boîte de réception)");
 });
-
 
 function submitForm(){
     // Initiate Variables With Form Content
@@ -26,27 +20,13 @@ function submitForm(){
     $.ajax({
         type: "POST",
         url: "http://singyourcreativity.com/fr/php/form-process-fr.php",
-        data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone,
-        success : function(text){
-            if (text == "passed"){
-                formSuccess();
-                sendConfEmail(message, name, email, phone);
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
+        data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
     });
-}
 
-function formSuccess(){
-    $("#contactFormFR")[0].reset();
-    submitMSG(true, "Merci. Nous avons envoyé une confirmation à votre adresse e-mail. (Vérifiez le dossier spam si vous ne le voyez pas dans votre boîte de réception)");
-}
-
-function formError(){
-    $("#contactFormFR").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
+    $.ajax({
+        type: "POST",
+        url: "http://singyourcreativity.com/fr/php/contact-conf-email-fr.php",
+        data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
     });
 }
 
@@ -57,12 +37,4 @@ function submitMSG(valid, msg){
         var msgClasses = "alert alert-danger text-center col-sm-10 col-sm-offset-2";
     }
     $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
-
-function sendConfEmail(message, name, email, phone){
-  $.ajax({
-      type: "POST",
-      url: "http://singyourcreativity.com/fr/php/contact-conf-email-fr.php",
-      data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
-  });
 }
